b55238c63a6c1879364c6ec68f12b08f0a2d8a54
Tyler Phelan <tphelan@pivotal.io>
2019-05-06 15:56:35 -0400

    Remove repeat ZipFiles code
    - Use imported method
    
    [#164880949]
    
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- creator/creator.go
+++ creator/creator.go
@@ -1,14 +1,15 @@
 package creator
 
 import (
-	"archive/zip"
 	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
-	"strings"
+
+	"github.com/cloudfoundry/libbuildpack/packager"
 
 	"github.com/cloudfoundry/cnb2cf/utils"
 
@@ -35,7 +36,19 @@ func CreateBuildpack(cfg Config, outputDir string) error {
 
 func CreateZip(config Config, srcDir, outputDir string) error {
 	bpZip := filepath.Join(outputDir, fmt.Sprintf("%s_buildpack-%s-%s.zip", config.Language, config.Stack, config.Version))
-	return zipFiles(srcDir, bpZip)
+	log.Printf("Creating shimmed buildpack at: %s", bpZip)
+
+	m := metadata.ManifestYAML{}
+	if err := m.Load(filepath.Join(srcDir, "manifest.yml")); err != nil {
+		return err
+	}
+
+	files := []packager.File{}
+	for _, name := range m.IncludeFiles {
+		files = append(files, packager.File{name, filepath.Join(srcDir, name)})
+	}
+
+	return packager.ZipFiles(bpZip, files)
 }
 
 func writeVersion(version, outputDir string) error {
@@ -113,50 +126,4 @@ func generateOrderTOML(cfg Config, outputDir string) error {
 	defer orderTOMLFile.Close()
 
 	return toml.NewEncoder(orderTOMLFile).Encode(orderTOML)
-}
-
-func zipFiles(srcDir, filename string) error {
-	newfile, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer newfile.Close()
-
-	zipWriter := zip.NewWriter(newfile)
-	defer zipWriter.Close()
-
-	return filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		header, err := zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
-
-		header.Name = filepath.Join(".", strings.TrimPrefix(path, srcDir))
-		header.Method = zip.Deflate
-
-		writer, err := zipWriter.CreateHeader(header)
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		_, err = io.Copy(writer, file)
-		return err
-	})
 }
